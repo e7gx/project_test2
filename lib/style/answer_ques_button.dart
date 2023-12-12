2a3e7f680312df29cc17525ee650dc56075dc1fd
e7gx <abalgabo@gmail.com>
2023-12-12 20:11:21 +0300

    debug some problems

--- lib/style/answer_ques_button.dart
+++ lib/style/answer_ques_button.dart
@@ -19,9 +19,8 @@ class AnswerButton extends StatelessWidget {
           vertical: 10,
           horizontal: 40,
         ),
-        textStyle: const TextStyle(
-          color: Colors.black,
-        ),
+        textStyle:
+            const TextStyle(color: Colors.black, fontWeight: FontWeight.w600),
         foregroundColor: Colors.black87,
         backgroundColor: Colors.white,
         shape: RoundedRectangleBorder(
